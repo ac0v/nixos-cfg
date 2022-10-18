bc8704d3a71f7bb522396f651d2e4e9dd56bd25b
ac0v <ac0v@sys-network.de>
2022-10-18 07:13:54 +0200

    hello blacky

---
+++ hosts/blacky/default.nix
@@ -0,0 +1,90 @@
+{ pkgs, config, lib, ... }:
+{
+  imports = [
+    ../home.nix
+    ./hardware-configuration.nix
+  ];
+
+  ## Modules
+  modules = {
+    desktop = {
+      i3.enable = true;
+      apps = {
+        rofi.enable = true;
+        # godot.enable = true;
+      };
+      browsers = {
+        default = "chrome";
+        chrome.enable = true;
+        firefox.enable = true;
+      };
+      gaming = {
+        # steam.enable = true;
+        # emulators.enable = true;
+        # emulators.psx.enable = true;
+      };
+      media = {
+        daw.enable = true;
+        documents.enable = true;
+        graphics.enable = true;
+        mpv.enable = true;
+        recording.enable = true;
+        spotify.enable = true;
+      };
+      term = {
+        default = "alacritty";
+        alacritty.enable = true;
+      };
+      vm = {
+        qemu.enable = true;
+      };
+    };
+    dev = {
+      node.enable = true;
+      rust.enable = true;
+      # python.enable = true;
+    };
+    editors = {
+      default = "nano";
+      emacs = {
+        enable = true;
+        doom.enable = true;
+      };
+      nano.enable = true;
+      vim.enable = true;
+    };
+    shell = {
+      adl.enable = true;
+      # vaultwarden.enable = true;
+      direnv.enable = true;
+      git.enable    = true;
+      gnupg.enable  = true;
+      tmux.enable   = true;
+      zsh.enable    = true;
+    };
+    services = {
+      ssh.enable = true;
+      docker.enable = true;
+      # onedrive.enable = true;
+    };
+    theme.active = "alucard";
+  };
+
+
+  ## Local config
+  programs.ssh.startAgent = true;
+  services.openssh.startWhenNeeded = true;
+
+  networking.networkmanager.enable = true;
+
+  boot = {
+    loader = {
+      grub = {
+        enable = true;
+        version = 2;
+        device = "/dev/nvme0n1";
+      };
+      systemd-boot.enable = false;
+    };
+  };
+}
